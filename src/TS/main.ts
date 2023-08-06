dc696ba5d0af9f15881a5fa170a8adf6eb8f5ae3
Djordje Trajkovic <djordje.traj20@gmail.com>
2023-08-06 22:24:53 +0200

    Uvodjenje podatka za coach-a, pravilan prikaz i booking

--- src/TS/main.ts
+++ src/TS/main.ts
@@ -16,12 +16,6 @@ function getDayOfWeek(currentDate: Date, i: number): Date {
   return nextDay;
 }
 
-let currentMonday = dayjs(getCurrentWeekMonday(new Date())).format(
-  "DD/MM/YYYY"
-);
-
-console.log(currentMonday);
-
 let calendarWeekDays = document.getElementById("calendar--weekday");
 let calendarHours = document.getElementById("calendar--hours");
 
@@ -35,7 +29,7 @@ let WeekDays = [
   "Sunday",
 ];
 
-type Booking = {
+type Bookings = {
   dayId: number;
   bookings: number[];
 };
@@ -44,28 +38,34 @@ type Schedule = {
   days: number[];
   startHour: number;
   endHour: number;
-  bookings: Booking[];
+  bookings: Bookings[];
 };
 
 const coach: Schedule = {
-  days: [1, 2],
+  days: [1, 2, 4, 5],
   startHour: 9,
   endHour: 17,
   bookings: [
     { dayId: 1, bookings: [] },
-    { dayId: 2, bookings: [] },
+    { dayId: 2, bookings: [12] },
     { dayId: 3, bookings: [] },
     { dayId: 4, bookings: [16] },
-    { dayId: 5, bookings: [] },
+    { dayId: 5, bookings: [13,14,15] },
     { dayId: 6, bookings: [] },
     { dayId: 7, bookings: [] },
   ],
 };
 
-const createCalendar = (days: number) => {
-  let calendarMatrix: String[7][23];
-  
-  for (let i = 0; i <= days; i++) {
+const doesContain = (array: number[], predicate: number) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === predicate) {
+      return predicate;
+    }
+  }
+};
+
+const createCalendar = (coach: Schedule) => {
+  for (let i = 0; i <= 7; i++) {
     let currentDate = getDayOfWeek(getCurrentWeekMonday(new Date()), i - 1);
     for (let j = 0; j < 24; j++) {
       if (i === 0) {
@@ -78,27 +78,47 @@ const createCalendar = (days: number) => {
       if (i !== 0 && j === 0) {
         //Za skroz gornji red
         let calendarWeekDay = document.createElement("div");
-        let calendarField = document.createElement("div")
+        let calendarField = document.createElement("div");
 
         calendarWeekDay.setAttribute("id", String(i));
-        calendarWeekDay.classList.add("column")
+        calendarWeekDay.classList.add("column");
         calendarField.classList.add("field");
         calendarField.innerText = WeekDays[i - 1];
         calendarWeekDays?.appendChild(calendarWeekDay);
-        calendarWeekDay.appendChild(calendarField)
+        calendarWeekDay.appendChild(calendarField);
       }
-      console.log(i, j);
 
       let calendarField = document.createElement("div");
       let calendarWeekDay = document.getElementById(String(i));
       calendarField.innerText = String(i) + " " + String(j);
       calendarField.classList.add(
         "field",
+        String(j),
         dayjs(currentDate).format("DD/MM/YYYY")
       );
+
+      if (
+        doesContain(coach.days, i) &&
+        j >= coach.startHour &&
+        j < coach.endHour &&
+        j != doesContain(coach.bookings[i - 1].bookings, j)
+      ) {
+        calendarField.addEventListener("click", () => {
+          calendarField.style.backgroundColor = "#0F766EB2";
+          coach.bookings[i - 1].bookings.push(j);
+          console.log(coach);
+        });
+      } else if (
+        i > 0 &&
+        j === doesContain(coach.bookings[i - 1].bookings, j)
+      ) {
+        calendarField.style.backgroundColor = "yellow";
+      } else {
+        calendarField.style.backgroundColor = "red";
+      }
+
       calendarWeekDay?.appendChild(calendarField);
     }
   }
 };
-
-createCalendar(7);
+createCalendar(coach);
