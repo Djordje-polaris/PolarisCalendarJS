a4efed19103cd340b31412bd64a90a9f7aebf102
Djordje-polaris <138766009+Djordje-polaris@users.noreply.github.com>
2023-08-11 16:08:11 +0200

    Rad na logici za local storage, funkcija brisanja null booking-a..

--- src/TS/main.ts
+++ src/TS/main.ts
@@ -1,5 +1,6 @@
 import dayjs, { Dayjs } from "dayjs";
 import weekOfYear from "dayjs/plugin/weekOfYear";
+import { json } from "stream/consumers";
 dayjs.extend(weekOfYear);
 
 // let calendarWeekDays = document.getElementById("calendar--weekdays");
@@ -11,6 +12,45 @@ let today = new Date();
 let chosenDate = new Date();
 let chosenWeek = dayjs(chosenDate).week();
 
+type Booking = {
+  hourId: number;
+  status: boolean | null;
+};
+
+type Bookings = {
+  dayId: number;
+  bookings: Booking[];
+};
+
+type Schedule = {
+  days: number[];
+  startHour: number;
+  endHour: number;
+  bookings: Bookings[];
+};
+
+let coach: Schedule = {
+  days: [1, 2, 4, 5],
+  startHour: 9,
+  endHour: 17,
+  bookings: [
+    { dayId: 1, bookings: [] },
+    { dayId: 2, bookings: [] }, //{ hourId: 12, status: null }
+    { dayId: 3, bookings: [] },
+    { dayId: 4, bookings: [] },
+    { dayId: 5, bookings: [] },
+    { dayId: 6, bookings: [] },
+    { dayId: 7, bookings: [] },
+  ],
+};
+
+const loadCoachData = () => {
+  const localData = localStorage.getItem(String(chosenWeek));
+  if (localData) {
+    coach = JSON.parse(localData);
+  }
+};
+
 if (calendarMonth && calendarYear) {
   calendarMonth.innerText = dayjs(chosenDate).format("MMMM");
   calendarYear.innerText = dayjs(chosenDate).format("YYYY");
@@ -18,7 +58,7 @@ if (calendarMonth && calendarYear) {
 
 function getCurrentWeekMonday(currentDate: Date): Date {
   const dayOfWeek = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
-  const daysUntilMonday = (dayOfWeek + 6) % 7; // Number of days to the nearest past Monday
+  const daysUntilMonday = ((dayOfWeek + 6) % 7) + 1; // Number of days to the nearest past Monday
 
   const currentWeekMonday = new Date(currentDate);
   currentWeekMonday.setDate(currentDate.getDate() - daysUntilMonday);
@@ -50,10 +90,44 @@ const doesContainBooking = (array: Booking[], predicate: number) => {
   return false;
 };
 
+let navigateCalendar = (currentDate: Date, flag: number): Date => {
+  if (flag) {
+    //forward
+    currentDate.setDate(currentDate.getDate() + 7);
+  } else {
+    //backward
+    if (currentDate <= today) {
+      return chosenDate;
+    }
+    currentDate.setDate(currentDate.getDate() - 7);
+  }
+  if (calendarMonth && calendarYear) {
+    calendarMonth.innerText = dayjs(currentDate).format("MMMM");
+    calendarYear.innerText = dayjs(currentDate).format("YYYY");
+  }
+  chosenWeek = dayjs(chosenDate).week();
+  console.log(chosenWeek, currentDate);
+  return currentDate;
+};
+
+let calendarNavigationForward = document.getElementById("go-forward");
+calendarNavigationForward?.addEventListener("click", () => {
+  chosenDate = navigateCalendar(chosenDate, 1);
+  loadCoachData();
+  fillCalendar(coach);
+});
+
+let calendarNavigationBackward = document.getElementById("go-back");
+calendarNavigationBackward?.addEventListener("click", () => {
+  chosenDate = navigateCalendar(chosenDate, 0);
+  loadCoachData();
+  fillCalendar(coach);
+});
+
 const checkBookingStatus = (
   array: Booking[],
   predicate: number,
-  status: boolean
+  status: boolean | null
 ) => {
   for (let i = 0; i < array.length; i++) {
     if (array[i].status === status && array[i].hourId === predicate) {
@@ -63,93 +137,27 @@ const checkBookingStatus = (
   return false;
 };
 
-let navigateCalendar = (currentDate: Date, flag: number): Date => {
-  if (flag) {
-    //forward
-    currentDate.setDate(currentDate.getDate() + 7);
-    chosenWeek = dayjs(chosenDate).week();
-  } else {
-    //backward
-    if (currentDate <= today) {
-      return chosenDate;
+const spliceBooking = (coach: Schedule) => {
+  for (let i = 0; i < coach.bookings.length; i++) {
+    for (let j = 0; j < coach.bookings[i].bookings.length; j++) {
+      if (coach.bookings[i].bookings[j].status === null)
+        coach.bookings[i].bookings.splice(j, 1);
     }
-    currentDate.setDate(currentDate.getDate() - 7);
-    chosenWeek = dayjs(chosenDate).week();
   }
-  if (calendarMonth && calendarYear) {
-    calendarMonth.innerText = dayjs(currentDate).format("MMMM");
-    calendarYear.innerText = dayjs(currentDate).format("YYYY");
-  }
-
-  console.log(chosenWeek, currentDate);
-  return currentDate;
-};
-
-let calendarNavigationForward = document.getElementById("go-forward");
-calendarNavigationForward?.addEventListener("click", () => {
-  chosenDate = navigateCalendar(chosenDate, 1);
-  console.log(coach);
-  fillCalendar(coach);
-  console.log(coach);
-});
-
-let calendarNavigationBackward = document.getElementById("go-back");
-calendarNavigationBackward?.addEventListener("click", () => {
-  navigateCalendar(chosenDate, 0);
-  fillCalendar(coach);
-});
-
-type Booking = {
-  hourId: number;
-  status: boolean | null;
-};
-
-type Bookings = {
-  dayId: number;
-  bookings: Booking[];
-};
-
-type Schedule = {
-  days: number[];
-  startHour: number;
-  endHour: number;
-  bookings: Bookings[];
-};
-
-const coach: Schedule = {
-  days: [1, 2, 4, 5],
-  startHour: 9,
-  endHour: 17,
-  bookings: [
-    { dayId: 1, bookings: [] },
-    { dayId: 2, bookings: [{ hourId: 12, status: true }] },
-    { dayId: 3, bookings: [] },
-    { dayId: 4, bookings: [{ hourId: 16, status: false }] },
-    {
-      dayId: 5,
-      bookings: [
-        { hourId: 13, status: true },
-        { hourId: 14, status: false },
-      ],
-    },
-    { dayId: 6, bookings: [] },
-    { dayId: 7, bookings: [] },
-  ],
 };
 
 function event(calendarField: HTMLDivElement, i: number, j: number) {
   calendarField.style.backgroundColor = "#0F766EB2";
   coach.bookings[i - 1].bookings.push({ hourId: j, status: false });
+  localStorage.setItem(String(chosenWeek), JSON.stringify(coach));
 }
 
 const createCalendar = (coach: Schedule) => {
+  spliceBooking(coach);
+
   for (let i = 1; i <= 7; i++) {
     let currentDate = getDayOfWeek(getCurrentWeekMonday(chosenDate), i);
-    console.log("Day: V", i);
-
     for (let j = 0; j < 24; j++) {
-      console.log("Hour: V", j);
-
       let calendarWeekDay = document.getElementById(String(i));
       let calendarField = document.createElement("div");
 
@@ -163,18 +171,18 @@ const createCalendar = (coach: Schedule) => {
 
       calendarField.classList.add("field");
 
-      console.log(
-        "doesContain(coach.days, i) ",
-        doesContain(coach.days, i),
-        " currentDate > today ",
-        currentDate > today,
-        " j >= coach.startHour ",
-        j >= coach.startHour,
-        " j < coach.endHour ",
-        j < coach.endHour,
-        " doesContain(coach.bookings[i - 1].bookings, j) ",
-        doesContainBooking(coach.bookings[i - 1].bookings, j)
-      );
+      // console.log(
+      //   "doesContain(coach.days, i) ",
+      //   doesContain(coach.days, i),
+      //   " currentDate > today ",
+      //   currentDate > today,
+      //   " j >= coach.startHour ",
+      //   j >= coach.startHour,
+      //   " j < coach.endHour ",
+      //   j < coach.endHour,
+      //   " doesContain(coach.bookings[i - 1].bookings, j) ",
+      //   doesContainBooking(coach.bookings[i - 1].bookings, j)
+      // );
 
       if (
         doesContain(coach.days, i) &&
@@ -183,13 +191,12 @@ const createCalendar = (coach: Schedule) => {
         j < coach.endHour && //Current hour is earlier than coaches ending hour
         !doesContainBooking(coach.bookings[i - 1].bookings, j) //Current hour is not a booked term
       ) {
-        console.log("Dodao event listener");
-
+        calendarField.style.backgroundColor = "white";
         calendarField.addEventListener("click", () =>
           event(calendarField, i, j)
         );
       } else if (
-        i > 0 && //If we are in week day range (Monday to Sunday)
+        currentDate > today &&
         doesContainBooking(coach.bookings[i - 1].bookings, j) //If current hour is a booked term
       ) {
         if (checkBookingStatus(coach.bookings[i - 1].bookings, j, true))
@@ -207,12 +214,11 @@ const createCalendar = (coach: Schedule) => {
 };
 
 const fillCalendar = (coach: Schedule) => {
+  spliceBooking(coach);
+  localStorage
   for (let i = 1; i <= 7; i++) {
-    console.log("Day V: ", i);
     let currentDate = getDayOfWeek(getCurrentWeekMonday(chosenDate), i);
     for (let j = 0; j < 24; j++) {
-      console.log("Hour: V", j);
-
       let calendarWeekDay = document.getElementById(String(i));
 
       if (calendarWeekDay) {
@@ -227,23 +233,23 @@ const fillCalendar = (coach: Schedule) => {
         );
 
         // Clone the node to remove listeners and attributes
-        const newField = calendarField.cloneNode(true) as HTMLDivElement;
+        let newField = calendarField.cloneNode(true) as HTMLDivElement;
         calendarField.parentNode?.replaceChild(newField, calendarField);
 
         newField.innerText = dayjs(currentDate).format("DD/MM/YYYY");
 
-        console.log(
-          "doesContain(coach.days, i) ",
-          doesContain(coach.days, i),
-          " currentDate > today ",
-          currentDate > today,
-          " j >= coach.startHour ",
-          j >= coach.startHour,
-          " j < coach.endHour ",
-          j < coach.endHour,
-          " doesContain(coach.bookings[i - 1].bookings, j) ",
-          doesContainBooking(coach.bookings[i - 1].bookings, j)
-        );
+        // console.log(
+        //   "doesContain(coach.days, i) ",
+        //   doesContain(coach.days, i),
+        //   " currentDate > today ",
+        //   currentDate > today,
+        //   " j >= coach.startHour ",
+        //   j >= coach.startHour,
+        //   " j < coach.endHour ",
+        //   j < coach.endHour,
+        //   " doesContain(coach.bookings[i - 1].bookings, j) ",
+        //   doesContainBooking(coach.bookings[i - 1].bookings, j)
+        // );
 
         if (
           doesContain(coach.days, i) && //if current day is a working day
@@ -252,12 +258,11 @@ const fillCalendar = (coach: Schedule) => {
           j < coach.endHour && //Current hour is earlier than coaches ending hour
           !doesContainBooking(coach.bookings[i - 1].bookings, j) //Current hour is not a booked term
         ) {
-          console.log("Dodao event listener");
           newField.style.backgroundColor = "white";
 
           newField.addEventListener("click", () => event(newField, i, j));
         } else if (
-          i > 0 && //If we are in week day range (Monday to Sunday)
+          currentDate > today &&
           doesContainBooking(coach.bookings[i - 1].bookings, j) //If current hour is a booked term
         ) {
           if (checkBookingStatus(coach.bookings[i - 1].bookings, j, true))
